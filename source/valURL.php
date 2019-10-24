7612d7a6b779b4bcfe1e409829a87b672e2c1546
= <=>
2019-10-24 08:26:26 +0700

    Initial Commit

--- source/valURL.php
+++ source/valURL.php
@@ -19,16 +19,16 @@ public function checkURRL($url){
         return false;}
 }
 
-public function subString($url1,$url2){
-    $urlSub1 = substr($url1,-1);
-    $urlSub2 = substr($url2,0,1);
+public function subString($url1 , $url2){
+    $urlSub1 = substr($url1, -1);
+    $urlSub2 = substr($url2, 0, 1);
 
-    $url_parse1 = parse_url($url1,PHP_URL_PATH);
-    $url_rep = str_replace($url_parse1,"",$url1);
+    $url_parse1 = parse_url($url1, PHP_URL_PATH);
+    $url_rep = str_replace($url_parse1, "", $url1);
 
     if($urlSub1 == "/" && $urlSub2 == "/"){
         return $url_rep.$url2;
-    } elseif ($urlSub1 != "/" && $urlSub2 != "/"){
+    } elseif ($urlSub1 != "/"  &&  $urlSub2 != "/"){
         return $url1."/".$url2;
     } else {
         return $url1.$url2;
@@ -40,10 +40,10 @@ public function protocol($url){
     return parse_url($url, PHP_URL_SCHEME);
 }
 public function port($url){
-    return parse_url($url,PHP_URL_PORT);
+    return parse_url($url, PHP_URL_PORT);
 }
 public function domain($url){
-    return parse_url($url,PHP_URL_HOST);
+    return parse_url($url, PHP_URL_HOST);
 }
 
 }
